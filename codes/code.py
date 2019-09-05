ce2d7f435b719ddbf70b616e8642ada2faddd5c8
minghanmilan <minghanmilan@gmail.com>
2019-09-04 17:17:39 -0700

    commit after large data file was added and committed

--- codes/code.py
+++ codes/code.py
@@ -2,6 +2,4 @@ import numpy as np
 import math
 from astropy.io import fits
 
-#modification1
-#modification2
-#modification in branch minghanmilan patch 2
+# this is a commit after the large data file was added and committed
